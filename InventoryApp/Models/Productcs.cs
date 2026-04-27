bbe4143cced5096da5e507f23b7bd9b091d1f0fb
Grosh69 <kormosabel2001@gmail.com>
2026-04-27 09:48:10 +0200

    Initial Web API project

---
+++ InventoryApp/Models/Productcs.cs
@@ -0,0 +1,15 @@
+﻿namespace InventoryApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
